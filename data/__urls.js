ca817c38e4ffc5e1ab5bf1c29f01062b3da0e153
non-beliefism <100057383+non-beliefism@users.noreply.github.com>
2022-02-19 22:32:46 -0500

    Add files via upload

---
+++ data/__urls.js
@@ -0,0 +1,278 @@
+//GENERAL
+
+//GENERAL/facebook
+function ___facebook ()
+{
+	window.open ("https://www.facebook.com/nonbeliefism/");
+}
+function ___folioverse ()
+{
+	window.open ("https://non-beliefism.github.io");
+}
+
+function ___book ()
+{
+	window.open ("https://www.researchgate.net/publication/319304479_Non_beliefism");
+}
+
+
+
+
+//INDEX
+function ___discord ()
+{
+	window.open ("https://discord.gg/2SQBhPn");
+}
+
+
+//PART i
+
+function ___part_i_1 ()
+{
+	window.open ("http://psr.sagepub.com/content/early/2013/08/02/1088868313497266.abstract");
+}
+function ___part_i_2 ()
+{
+	window.open ("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4327528/");
+}
+function ___part_i_3 ()
+{
+	window.open ("http://www.crossingdialogues.com/Ms-A14-03.pdf");
+}
+function ___part_i_4 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Absolute_space_and_time");
+}
+
+
+
+
+//PART ii
+function ___part_ii_0 ()
+{
+	window.open ("https://medium.com/@jordanmicahbennett/scientific-evidence-showing-beliefs-science-opposing-design-8248c83b1a1e");
+}
+function ___part_ii_1 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Absolute_space_and_time");
+}
+function ___part_ii_2 ()
+{
+	window.open ("https://www.researchgate.net/publication/318921599_Genius_Edward_Witten_could_he_help_to_intensify_artificial_intelligence_research");
+}
+
+function ___part_ii_3 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Science#Certainty_and_science");
+}
+
+
+
+
+
+
+
+//PART iv
+function ___part_iii_0 ()
+{
+	window.open ("http://bigthink.com/ideafeed/mit-physicist-proposes-new-meaning-of-life");
+}
+function ___part_iii_1 ()
+{
+	window.open ("https://www.youtube.com/watch?v=qv6UVOQ0F44");
+}
+function ___part_iii_2 ()
+{
+	window.open ("https://www.youtube.com/watch?v=P_-wn8ghcoY");
+}
+function ___part_iii_3 ()
+{
+	window.open ("https://www.youtube.com/watch?v=7Pq-S557XQU");
+}
+function ___part_iii_4 ()
+{
+	window.open ("https://www.youtube.com/watch?v=t4kyRyKyOpo");
+}
+function ___part_iii_5 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Exascale_computing");
+}
+function ___part_iii_6 ()
+{
+	window.open ("http://www.research.ibm.com/cognitive-computing/neurosynaptic-chips.shtml");
+}
+function ___part_iii_7 ()
+{
+	window.open ("http://people.uwplatt.edu/~yangq/csse411/csse411-materials/s13/cs_1/brostm_AI-in-commercet.doc");
+}
+function ___part_iii_8 ()
+{
+	window.open ("https://medium.com/@uni.omniscient.x/god-is-probably-quite-real-a466e9f24a0b#.yc5sk5is8");
+}
+function ___part_iii_9 ()
+{
+	window.open ("https://medium.com/@jordanmicahbennett/how-to-become-a-god-in-3-simple-steps-absent-faith-belief-f95f5c16f375#.iuje6oxb7");
+}
+function ___part_iii_10 ()
+{
+	window.open ("https://www.quora.com/What-is-the-meaning-of-life-85/answer/Jordan-Bennett-9");
+}
+function ___part_iii_11 ()
+{
+	window.open ("https://www.researchgate.net/publication/316662636_A_scientific_redefinition_of_God_by_an_atheist");
+}
+function ___part_iii_12 ()
+{
+	window.open ("https://www.researchgate.net/publication/316662636_A_scientific_redefinition_of_God_by_an_atheist");
+}
+function ___part_iii_13 ()
+{
+	window.open ("https://www.youtube.com/watch?v=8nt3edWLgIg");
+}
+function ___part_iii_14 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Cosmological_argument");
+}
+function ___part_iii_15 ()
+{
+	window.open ("https://www.youtube.com/watch?v=WSKi8HfcxEk");
+}
+function ___part_iii_16 ()
+{
+	window.open ("https://www.researchgate.net/publication/319235750_Life%27s_meaning");
+}
+function ___part_iii_17 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Exascale_computing");
+}
+function ___part_iii_18 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Models_of_scientific_inquiry");
+}
+function ___part_iii_19 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Neuroscience_and_intelligence#Brain_volume");
+}
+function ___part_iii_20 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Astrology_and_astronomy");
+}
+function ___part_iii_21 ()
+{
+	window.open ("https://www.youtube.com/watch?v=8nt3edWLgIg&feature=youtu.be&t=849");
+}
+function ___part_iii_22 ()
+{
+	window.open ("https://youtu.be/w86lf799MDY?t=73");
+}
+function ___part_iii_23 ()
+{
+	window.open ("http://mentalfloss.com/article/75519/meanings-behind-9-common-and-thankfully-not-so-common-disease-names");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PREMISE_SEQUENCE
+function ___premise_sequence_0 ()
+{
+	window.open ("https://medium.com/@uni.omniscient.x/stop-believing-in-everything-2cfbb65e352d#.7laowj8o0");
+}
+function ___premise_sequence_1 ()
+{
+	window.open ("https://drive.google.com/file/d/0B8H3Ghe4haTWNlF5MmkxV0dwTzg/view?usp=sharing");
+}
+function ___premise_sequence_2 ()
+{
+	window.open ("https://medium.com/@jordanmicahbennett/belief-is-entirely-non-necessary-1fa42c6c045f#.j5mkmhtmo");
+}
+function ___premise_sequence_3 ()
+{
+	window.open ("https://medium.com/@uni.omniscient.x/logic-is-illogical-c2738616e42#.f68hr7r20");
+}
+function ___premise_sequence_4 ()
+{
+	window.open ("https://www.youtube.com/watch?v=WtBnm0X50VQ");
+}
+function ___premise_sequence_5 ()
+{
+	window.open ("http://nobeliefs.com/problemswithbeliefs.htm");
+}
+function ___premise_sequence_6 ()
+{
+	window.open ("https://www.amazon.com/dp/B072TXZ18M/");
+}
+function ___premise_sequence_7 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Scientific_theory");
+}
+function ___premise_sequence_8 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Hypothesis");
+}
+function ___premise_sequence_9 ()
+{
+	window.open ("https://en.wikipedia.org/wiki/Testability");
+}
+ 
+
+//v2
+function _v2_item0 ( )
+{
+	window.open ("http://non-beliefism-info.appspot.com/");
+}
+function _v2_item1 ( )
+{
+	window.open ("https://en.oxforddictionaries.com/definition/belief");
+}
+function _v2_item2 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Science");
+}
+function _v2_item3 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Hypothesis#Scientific_hypothesis");
+}
+function _v2_item4 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Scientific_theory");
+}
+function _v2_item5 ( )
+{
+	window.open ("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4327528/");
+}
+function _v2_item6 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Neil_deGrasse_Tyson");
+}
+function _v2_item7 ( )
+{
+	window.open ("https://www.youtube.com/watch?v=WtBnm0X50VQ");
+}
+function _v2_item8 ( )
+{
+	window.open ("https://philarchive.org/rec/OFEMFA?all_versions=1");
+}
+function _v2_item9 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Teleonomy");
+}
+function _v2_item10 ( )
+{
+	window.open ("https://en.wikipedia.org/wiki/Neil_deGrasse_Tyson");
+}
+
+
+function _v2_item11 ( )
+{
+	window.open ("https://www.nobeliefs.com/problemswithbeliefs.htm");
+}
---
+++ data/javascript.js
@@ -0,0 +1,7 @@
+
+
+window.onload = function ( )
+{
+	//if ( screen.width < 1600)
+		//swal({title: "",text: "Mobile Beta  /Non Beliefism", confirmButtonColor: "#fff"});
+}
